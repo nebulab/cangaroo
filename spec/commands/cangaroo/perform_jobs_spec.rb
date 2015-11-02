6c663050be367d9e6f01a370808e7aa898746fb3
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-11-02 11:31:13 +0100

    adds more specs for PerformJobs command

--- spec/commands/cangaroo/perform_jobs_spec.rb
+++ spec/commands/cangaroo/perform_jobs_spec.rb
@@ -2,15 +2,8 @@ require 'rails_helper'
 
 module Cangaroo
   RSpec.describe PerformJobs do
-    class JobA < Job
-      def perform?(item); true; end
-      def enqueue; nil; end
-    end
-
-    class JobB < Job
-      def perform?(item); false; end
-      def enqueue; nil; end
-    end
+    class JobA < Job ; end
+    class JobB < Job ; end
 
     let(:command) { PerformJobs.new(item, [JobA, JobB]) }
     let(:item) { create(:cangaroo_item) }
@@ -20,12 +13,19 @@ module Cangaroo
     before do
       allow(JobA).to receive(:new).and_return(job_a)
       allow(JobB).to receive(:new).and_return(job_b)
+      allow(job_a).to receive_messages(perform?: true, enqueue: nil)
+      allow(job_b).to receive_messages(perform?: false, enqueue: nil)
     end
 
     describe '#call' do
       before { command.call }
 
       it 'enqueues only cangaroo jobs that can perform' do
+        expect(job_a).to have_received(:enqueue)
+        expect(job_b).to_not have_received(:enqueue)
+      end
+
+      it 'returns the enqueued jobs' do
         expect(command.result).to include(job_a)
         expect(command.result).to_not include(job_b)
       end
