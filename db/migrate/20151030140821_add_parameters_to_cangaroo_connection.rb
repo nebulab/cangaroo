602bfca5b85ba1be8ae2db33580a7b32dff49afc
Alessio Rocco <alessio.rocco.lt@gmail.com>
2015-11-18 15:53:54 +0100

    sets connection parameters default as {}

--- db/migrate/20151030140821_add_parameters_to_cangaroo_connection.rb
+++ db/migrate/20151030140821_add_parameters_to_cangaroo_connection.rb
@@ -1,5 +1,5 @@
 class AddParametersToCangarooConnection < ActiveRecord::Migration
   def change
-    add_column :cangaroo_connections, :parameters, :text
+    add_column :cangaroo_connections, :parameters, :text, default: {}
   end
 end
