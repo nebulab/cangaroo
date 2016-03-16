cadf17b318d69e787a54a1409a5d16bdc2909ac6
Michael Bianco <iloveitaly@gmail.com>
2016-03-16 19:55:15 -0400

    Set Connection#parameter default in `after_initialize` hook

--- app/models/cangaroo/connection.rb
+++ app/models/cangaroo/connection.rb
@@ -5,8 +5,16 @@ module Cangaroo
     validates :name, :url, :key, :token, presence: true
     validates :name, :url, :key, :token, uniqueness: true
 
+    after_initialize :set_default_parameters
+
     def self.authenticate(key, token)
       where(key: key, token: token).first
     end
+
+    private
+
+      def set_default_parameters
+        self.parameters ||= {}
+      end
   end
 end
