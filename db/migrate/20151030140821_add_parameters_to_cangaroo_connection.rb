2b0dbe20b52e27e528b451ede7456e68c9e471d9
Michael Bianco <iloveitaly@gmail.com>
2016-03-16 19:53:42 -0400

    Stop setting default parameters value in migration
    This caused a fatal error for me on postgres + rails5

--- db/migrate/20151030140821_add_parameters_to_cangaroo_connection.rb
+++ db/migrate/20151030140821_add_parameters_to_cangaroo_connection.rb
@@ -1,5 +1,5 @@
 class AddParametersToCangarooConnection < ActiveRecord::Migration
   def change
-    add_column :cangaroo_connections, :parameters, :text, default: {}
+    add_column :cangaroo_connections, :parameters, :text
   end
 end
