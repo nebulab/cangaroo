2dd65fd9cd0dc0b4b5424bff0798ba1361750230
Michael Bianco <iloveitaly@gmail.com>
2016-04-05 11:49:46 -0400

    Adding spec for poll jobs

---
+++ spec/jobs/cangaroo/poll_job_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe Cangaroo::PollJob, type: :job do
+  class FakePollJob < Cangaroo::PollJob
+    connection :store
+    path '/webhook_path'
+    parameters(email: 'info@nebulab.it')
+  end
+
+  let(:job_class) { FakePollJob }
+  let(:job) { job_class.new({ last_poll: Time.now.to_i }) }
+  let(:successful_pull_payload) { parse_fixture('json_payload_ok.json') }
+
+  before do
+    create(:cangaroo_connection)
+  end
+
+  describe '#perform' do
+
+    context 'pull is successful' do
+
+      before do
+        allow(Cangaroo::HandleRequest).to receive(:call).and_return(double(
+          success?: true
+        ))
+      end
+
+      it 'updates the poll timestamp' do
+        Cangaroo::Webhook::Client.any_instance.stub(:post).and_return(successful_pull_payload)
+
+        job.perform
+
+        last_poll_timestamp = Cangaroo::PollTimestamp.for_class(FakePollJob)
+        expect(last_poll_timestamp.job).to eq(job.class.to_s)
+        expect(last_poll_timestamp.connection.name).to eq(job.class.connection.to_s)
+        expect(last_poll_timestamp.value).to be <= DateTime.now
+      end
+
+      it 'handles a empty response' do
+        Cangaroo::Webhook::Client.any_instance.stub(:post).and_return('')
+
+        job.perform
+      end
+
+      it 'handles a response with a empty array' do
+        Cangaroo::Webhook::Client.any_instance.stub(:post).and_return(
+          parse_fixture('json_payload_empty.json')
+        )
+
+        job.perform
+      end
+
+    end
+
+    context 'pull fails' do
+
+      before do
+        Cangaroo::Webhook::Client.any_instance.stub(:post).and_return(parse_fixture('json_payload_ok.json'))
+
+        allow(Cangaroo::HandleRequest).to receive(:call).and_return(double(
+          success?: false,
+          message: 'bad failure'
+        ))
+      end
+
+      it 'does not update timestamp if pull fails' do
+        expect { job.perform }.to raise_error(Cangaroo::Webhook::Error)
+
+        expect(Cangaroo::PollTimestamp.for_class(FakePollJob).id).to be_nil
+      end
+
+    end
+  end
+
+end
