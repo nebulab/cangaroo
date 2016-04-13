a5643710a39fdf4f81c60330efa474dbc75d5008
Michael Bianco <iloveitaly@gmail.com>
2016-04-12 20:51:14 -0400

    Moving poll frequency check from RunPolls to PollJob
    The last poll parameter shouldn't be passed to the job, it
    should be read from the database at the time the job was run.

--- app/interactors/cangaroo/run_polls.rb
+++ app/interactors/cangaroo/run_polls.rb
@@ -3,15 +3,8 @@ module Cangaroo
     include Interactor
 
     def call
-      poll_execution_time = DateTime.now
-
       context.jobs.each do |poll_job|
-        last_poll_timestamp = Cangaroo::PollTimestamp.for_class(poll_job)
-        last_poll_date = last_poll_timestamp.value
-
-        if last_poll_date.nil? || poll_execution_time.to_i - last_poll_date.to_i > poll_job.frequency
-          poll_job.new(last_poll: last_poll_date.to_i).enqueue
-        end
+        poll_job.new.enqueue
       end
     end
 
--- app/jobs/cangaroo/poll_job.rb
+++ app/jobs/cangaroo/poll_job.rb
@@ -13,8 +13,13 @@ module Cangaroo
       last_poll = Time.at(arguments.first.fetch(:last_poll)).to_datetime
       current_time = DateTime.now
 
+      if !perform?(current_time)
+        log.info 'skipping poll'
+        return
+      end
+
       response = Cangaroo::Webhook::Client.new(destination_connection, path)
-        .post({ last_poll: last_poll.to_i }, @job_id, parameters)
+        .post({ last_poll: last_poll_timestamp.to_i }, @job_id, parameters)
 
       command = HandleRequest.call(
         key: destination_connection.key,
@@ -32,7 +37,16 @@ module Cangaroo
       last_job_poll.save!
     end
 
+    def perform?(execution_time)
+      last_poll_timestamp.nil? ||
+      execution_time.to_i - last_poll_timestamp.to_i > self.class.frequency
+    end
+
     protected
+
+      def last_poll_timestamp
+        @last_poll_timestamp ||= Cangaroo::PollTimestamp.for_class(self.class).value
+      end
 
       def destination_connection
         @connection ||= Cangaroo::Connection.find_by!(name: connection)
