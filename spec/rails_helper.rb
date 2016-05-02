4b0c589bdfbafe17408e4f83441722cb863ed23b
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-05-02 12:02:17 +0200

    Better organize spec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,58 +20,22 @@ end
 
 require 'spec_helper'
 require 'rspec/rails'
-require 'shoulda-matchers'
-require 'factory_girl_rails'
-require 'database_cleaner'
-require 'webmock/rspec'
 
-# Add additional requires below this line. Rails is not loaded until this point!
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
+# We manually require each file to make sure the files are required in the
+# correct order and no exceptions come up because of wrong require order
+%w(
+  database_cleaner
+  webmock
+  shoulda_matchers
+  factory_girl
+  spec_helpers
+).each { |path| require File.expand_path("../support/#{path}.rb", __FILE__) }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
-  # whitelist codeclimate.com so test coverage can be reported
-  config.after(:suite) do
-    WebMock.disable_net_connect!(allow: 'codeclimate.com')
-  end
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-
   # reset config before each spec
   config.before(:each) do
     Rails.configuration.cangaroo.basic_auth = false
