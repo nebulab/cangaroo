ed5aac9fc36264e2baa4d97dd5149abd23faee6f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-04-04 15:55:01 +0200

    Update codeclimate configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,9 @@
 require 'simplecov'
-require 'codeclimate-test-reporter'
 require 'pry-byebug'
 
 SimpleCov.start 'rails' do
   add_group 'Commands', 'app/commands'
 end
-CodeClimate::TestReporter.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
